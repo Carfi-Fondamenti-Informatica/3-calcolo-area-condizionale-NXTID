93d2e75e314bbd3865fffb0adf6d726b0f57d376
NXTID <117934144+NXTID@users.noreply.github.com>
2022-11-17 18:03:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0;
+    float b=0;
+    int opzione=0;
+    float area=0;
+
+    cin>>a>>b>>opzione;
+
+    switch(opzione){
+        case 0:
+            area=(a*b)/2;
+            cout<<area;
+            break;
+        case 1:
+            area=a*a;
+            cout<<area;
+            break;
+        case 2:
+            area=a*b;
+            cout<<area;
+            break;
+        default:
+            cout<<"opzione non valida";
+            break;
+    }
+
+    return 0;
 }
